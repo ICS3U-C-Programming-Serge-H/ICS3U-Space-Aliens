fc2731fb29fa96ddcda75a644eaa8261eb068174
Serge Hamouche <Serge.el-hamouche@stu.ocsb.ca>
2025-06-03 15:56:32 +0000

    commit RST.py

--- RST.py
+++ RST.py
@@ -26,15 +26,34 @@ def game_scene():
     game.layers = [ship] + [background]
     # render the backround and initial location of sprite list
     # most likely you will only render backround once per scene
-
     game.render_block()
-
-    print("Hello, Serge!")
-    print("Hello, welcome to my Domain!")
 
     # repeat forever, game loop
     while True:
+        # get user input
+        keys = ugame.buttons.get_pressed()
 
+        if keys & ugame.K_X:
+            print("A")
+        if keys & ugame.K_O:
+            print("B")
+        if keys & ugame.K_START:
+            print("Start")
+        if keys & ugame.K_SELECT:
+            print("Select")
+        if keys & ugame.K_RIGHT:
+            ship.move(ship.x + 1, ship.y)
+        if keys & ugame.K_LEFT:
+            ship.move(ship.x - 1, ship.y)
+        if keys & ugame.K_UP:
+            ship.move(ship.x, ship.y - 1)
+        if keys & ugame.K_DOWN:
+            ship.move(ship.x, ship.y + 1)
+
+       
+        # update game logic
+       
+        # redraw Sprites
         game.render_sprites([ship])
         # makes sure the ship is always on screen
         game.tick()
